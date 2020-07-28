176b02217c019dcfa5222d6070c2dbf92aae0337
Shivangi <shivangi.das7@gmail.com>
2020-07-28 20:45:57 +0100

    record time, refractor code

---
+++ Training/Readme.md
@@ -0,0 +1,9 @@
+Use instructions in download data for getting coco dataset
+
+Run following to start training
+
+python finetune.py
+
+Can use timeout
+
+timeout 4h python finetune.py
--- Training/finetune.py
+++ Training/finetune.py
@@ -9,37 +9,108 @@ import torchvision
 from torchvision import datasets, models, transforms
 import matplotlib.pyplot as plt
 import time
+import datetime
 import os
 import copy
+import uuid
 plt.ion()
-# Data augmentation and normalization for training
-# Just normalization for validation
-data_transforms = {
-    'train': transforms.Compose([
-        transforms.RandomResizedCrop(224),
-        transforms.RandomHorizontalFlip(),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ]),
-    'val': transforms.Compose([
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ]),
-}
 
-data_dir = '/media/data/shivangi/coco'
-image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
-                                          data_transforms[x])
-                  for x in ['train', 'val']}
-dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=4,
-                                              shuffle=True, num_workers=4)
-               for x in ['train', 'val']}
-dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
-class_names = image_datasets['train'].classes
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+def main():
+
+    # Data augmentation and normalization for training
+    # Just normalization for validation
+    data_transforms = {
+        'train': transforms.Compose([
+            transforms.RandomResizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+        'val': transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+    }
+    fileName = 'time.txt'
+    prep_file(fileName)
+    timefile = '../logs/' + fileName
+    start_time = datetime.datetime.now()
+    row = f"start_time={start_time}\n"
+    with open(timefile, "a") as file1:
+        file1.writelines(row)
+    end_time = datetime.datetime.now()
+    row = f"end_time={end_time}\n"
+
+    data_dir = '/media/data/shivangi/coco'
+    image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
+                                              data_transforms[x])
+                      for x in ['train', 'val']}
+    dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=4,
+                                                  shuffle=True, num_workers=4)
+                   for x in ['train', 'val']}
+    dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
+    class_names = image_datasets['train'].classes
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    # Get a batch of training data
+    inputs, classes = next(iter(dataloaders['train']))
+
+    # Make a grid from batch
+    out = torchvision.utils.make_grid(inputs)
+
+    # imshow(out, title=[class_names[x] for x in classes])
+    model_ft = models.vgg16(pretrained=True)
+
+    # num_ftrs = model_ft.fc.in_features
+    # Here the size of each output sample is set to 2.
+    # Alternatively, it can be generalized to nn.Linear(num_ftrs, len(class_names)).
+    num_ftrs = model_ft.classifier[0].out_features
+
+    # list all the modules in the model's classifier
+    mod = list(model_ft.classifier.children())
+    # Pop the last module and add 2 modules
+    mod.pop()
+    mod.append(nn.Linear(num_ftrs, 100))
+    mod.append(nn.Linear(100, len(class_names)))
+
+    # Replace vgg16's classifier with this new classifier
+    new_classifier = nn.Sequential(*mod)
+    model_ft.classifier = new_classifier
+
+    # model_ft.fc = nn.Linear(num_ftrs, len(class_names))
+
+    model_ft = model_ft.to(device)
+
+    criterion = nn.CrossEntropyLoss()
+
+    # Observe that all parameters are being optimized
+    optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
+
+    # Decay LR by a factor of 0.1 every 7 epochs
+    exp_lr_scheduler = lr_scheduler.StepLR(
+        optimizer_ft, step_size=7, gamma=0.1)
+
+    model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler,
+                           num_epochs=25)
+    end_time = datetime.datetime.now()
+    row = f"end_time={end_time}\n"
+    with open(timefile, "a") as file1:
+        file1.writelines(row)
+    # visualize_model(model_ft)
+
+
+def prep_file(fileName):
+    path = '../logs/' + fileName
+    if os.path.exists(path):
+        id = uuid.uuid4()
+        renameFile = '../logs/' + fileName.split(".")[0] + str(id) + '.csv'
+        os.rename(path, renameFile)
+    with open(path, "w") as file1:
+        file1.writelines("")
 
 
 def imshow(inp, title=None):
@@ -53,15 +124,6 @@ def imshow(inp, title=None):
     if title is not None:
         plt.title(title)
     plt.pause(0.001)  # pause a bit so that plots are updated
-
-
-# Get a batch of training data
-inputs, classes = next(iter(dataloaders['train']))
-
-# Make a grid from batch
-out = torchvision.utils.make_grid(inputs)
-
-# imshow(out, title=[class_names[x] for x in classes])
 
 
 def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
@@ -160,36 +222,5 @@ def visualize_model(model, num_images=6):
         model.train(mode=was_training)
 
 
-model_ft = models.vgg16(pretrained=True)
-# num_ftrs = model_ft.fc.in_features
-# Here the size of each output sample is set to 2.
-# Alternatively, it can be generalized to nn.Linear(num_ftrs, len(class_names)).
-num_ftrs = model_ft.classifier[0].out_features
-
-# list all the modules in the model's classifier
-mod = list(model_ft.classifier.children())
-# Pop the last module and add 2 modules
-mod.pop()
-mod.append(nn.Linear(num_ftrs, 100))
-mod.append(nn.Linear(100, len(class_names)))
-
-# Replace vgg16's classifier with this new classifier
-new_classifier = nn.Sequential(*mod)
-model_ft.classifier = new_classifier
-
-
-# model_ft.fc = nn.Linear(num_ftrs, len(class_names))
-
-model_ft = model_ft.to(device)
-
-criterion = nn.CrossEntropyLoss()
-
-# Observe that all parameters are being optimized
-optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
-
-# Decay LR by a factor of 0.1 every 7 epochs
-exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
-
-model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler,
-                       num_epochs=25)
-# visualize_model(model_ft)
+if __name__ == "__main__":
+    main()
